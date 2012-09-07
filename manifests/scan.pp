6068bed36d9b131f8bcd4dd0e95f9f0c1262de86
Adam Jahn <ajjahn@gmail.com>
2012-09-07 11:53:24 -0400

    ignore clipping warning

--- manifests/scan.pp
+++ manifests/scan.pp
@@ -3,9 +3,10 @@ define replaygain::scan ($interval = 1, $ensure = present) {
   $directory = $name
 
   cron { "replaygain-${directory}":
-    command => "find ${directory} -iname *.m4a | while read file; do aacgain -r -e \"\$file\"; done; find ${directory} -iname *.mp3 | while read file; do mp3gain -r -e \"\$file\"; done",
+    command => "find ${directory} -iname *.m4a | while read file; do aacgain -r -e -c \"\$file\"; done; find ${directory} -iname *.mp3 | while read file; do mp3gain -r -e -c \"\$file\"; done",
     user => 'root',
     hour => "*/${interval}",
+    miniute => absent
     ensure => $ensure,
     require => Class['replaygain::install'],
   }
